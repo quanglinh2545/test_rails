eb8be1139c00eff81ae5a8dadcc7d441f82d45a6
Ubuntu <ubuntu@ip-172-31-49-216.ec2.internal>
2020-11-29 16:49:38 +0000

    Transition to manga

--- app/controllers/mangas_controller.rb
+++ app/controllers/mangas_controller.rb
@@ -1,5 +1,6 @@
 class MangasController < ApplicationController
   before_action :set_manga, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /mangas
   # GET /mangas.json
